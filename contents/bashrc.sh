611190df9b867ce2b29e608176703b49b4ad5a42
Kevin Littlejohn <kevin@littlejohn.id.au>
2022-09-10 11:09:05 +0000

    list what's available at login

--- contents/bashrc.sh
+++ contents/bashrc.sh
@@ -14,10 +14,13 @@ sudo /usr/local/bin/fix_docker.sh
 # Fix X11
 . /usr/local/bin/fix_x11.sh
 
+# List installable components
+/usr/local/bin/install.sh --list
+
 # User specific aliases and functions
 alias login=". /usr/local/bin/login.sh"
 echo ". login <accountname> <rolename> for AWS login"
 echo "~/.aws/aliases.sh on the host can be used to extend the list of aliases if needed"
-echo "cat /CONTENTS.md for tips on what's available on here"
 [[ -x ~/.aws/aliases.sh ]] && . ~/.aws/aliases.sh && alias | grep login
+echo "cat /CONTENTS.md for tips on what's available on here"
 echo
