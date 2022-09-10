d0acb930fd26118b8a41a19aa3b11c874e02b808
Kevin Littlejohn <kevin@littlejohn.id.au>
2022-09-10 08:41:28 +0000

    simpler fix for x11

--- contents/fix_x11.sh
+++ contents/fix_x11.sh
@@ -3,17 +3,10 @@
 # vscode on windows in wsl does this, haven't tracked down why yet
 if [[ ${DISPLAY} == "1" ]]; then export DISPLAY=:0; fi
 
-if ! xset q >/dev/null; then
-    # If we have a HOSTNAME var, use that to generate DISPLAY = ${HOSTNAME}:0
-    if [[ -n ${HOSTNAME} ]]; then
-        export DISPLAY=${HOSTNAME}:0
-        if xset q >/dev/null; then
-            export DISPLAY=${HOSTNAME}.local:0
-            if xset q >/dev/null; then
-                echo "X11 not working, not sure how to fix (tried HOSTNAME)"
-            fi
-        fi
-    else
-        echo "X11 not working, not sure how to fix (no HOSTNAME)"
+if ! xset q >/dev/null 2>&1; then
+    export DISPLAY=host.docker.internal:0
+    if ! xset q >/dev/null 2>&1; then
+        echo "X11 not working for ${DISPLAY}, try 'xhost +localhost' in host"
     fi
 fi
+echo "DISPLAY=${DISPLAY}"
